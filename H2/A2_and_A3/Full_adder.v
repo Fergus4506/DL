d2b7bf50c7af2cf622edec9b24680186866a1594
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-13 18:53:57 +0800

    chage file name

---
+++ H2/A2_and_A3/Full_adder.v
@@ -0,0 +1,17 @@
+module Full_adder(
+        input x,y,z,
+        output S,C
+    );
+    wire [7:0] D_wire;
+
+    Decoder_3x8 uut(
+        .D(D),
+        .x(x),
+        .y(y),
+        .z(z)
+    );
+
+    S=D_wire[1]||D_wire[2]||D_wire[4]||D_wire[7];
+    C=D_wire[3]||D_wire[5]||D_wire[6]||D_wire[7];
+
+endmodule
