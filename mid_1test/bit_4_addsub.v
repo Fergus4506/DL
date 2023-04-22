0dd848b539f5c7dc02d49a2b7cd524aaa7ba3499
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-22 14:33:26 +0800

    bit_4_addsub check

--- mid_1test/bit_4_addsub.v
+++ mid_1test/bit_4_addsub.v
@@ -6,11 +6,18 @@ module bit_4_addsub(
     );
     wire C1,C2,C3;
     wire [3:0]BXor;
-    assign BXor[0]=B[0]^M,
-        BXor[1]=B[1]^M,
-        BXor[2]=B[2]^M,
-        BXor[3]=B[3]^M,
-        V=C3^C;
+    // assign BXor[0]=B[0]^M,
+    //     BXor[1]=B[1]^M,
+    //     BXor[2]=B[2]^M,
+    //     BXor[3]=B[3]^M,
+    //     V=C3^C;
+
+    xor G1(BXor[0],M,B[0]);
+	xor G2(BXor[1],M,B[1]);
+	xor G3(BXor[2],M,B[2]);
+	xor G4(BXor[3],M,B[3]);
+	xor G5(V,C3,C);
+
     Full_adder 
         FA1(S[0],C1,A[0],BXor[0],M),
         FA2(S[1],C2,A[1],BXor[1],C1),
--- mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/xsim_webtalk.tcl
+++ mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/DL/mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Apr 22 14:15:50 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Apr 22 14:31:55 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "b40dfb1e-1edd-4a0f-b3ce-1d0d7ae692fd" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "a0a9665bd24244f1b279d43b99e89972" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "12" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "16" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-9400F CPU @ 2.90GHz" -context "user_environment"
@@ -27,6 +27,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "200 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6640_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4226244057 -regid "212716944_0_0_342" -xml D:/DL/mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/DL/mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/DL/mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6648_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2717375964 -regid "212716944_0_0_342" -xml D:/DL/mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/DL/mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/DL/mid_1test/mid1/mid1.sim/sim_1/behav/xsim/xsim.dir/t_bit_4_addsub_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
