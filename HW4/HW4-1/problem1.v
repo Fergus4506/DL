6093a0812813ab7662933e4f3053a274dbdcf4d1
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-06-08 17:34:42 +0800

    [upload] HW4-1 temp Ok

--- HW4/HW4-1/problem1.v
+++ HW4/HW4-1/problem1.v
@@ -4,35 +4,35 @@ module problem1(A,B,in_B,control,reset,clk);
     input in_B;
     input reset,clk,control;
     reg [3:0]t_A,t_B;
-    reg C,S;
+    reg C;
+    wire S;
     wire con_clk;
     assign x=t_A[0],
            y=t_B[0],
-           con_clk=control&clk;
+           con_clk=control&clk,
+           S=x^y^C;
 
-    always@(posedge control)
-        t_A<=A;
+    // always@(posedge control)
+    //     t_A<=A;
         
-    always@(posedge control)
-        t_B<=B;
-
-    always@(posedge con_clk,negedge reset)
-        if(~reset)S<=0;
-        else S<=(C^x^y);
+    // always@(posedge control)
+    //     t_B<=B;
 
     always@(posedge con_clk,negedge reset)
         if(~reset)C<=1'b0;
         else if(C==1'b0)
-            C<=t_A[0]^t_B[0];
+            C<=t_A[0]&t_B[0];
         else
-            C<=~(t_A[0]^t_B[0]);
+            C<=~(t_A[0]&t_B[0]);
             
     always@(posedge con_clk,negedge control)
-        if(~control)t_A<=t_A;
+        if((~control)&(~reset))t_A<=4'b0000;//這行要放什麼是關鍵(是否需設定暫存器目前存取值 t_A<=A)
+        else if(~control)t_A<=t_A;
         else if(S) t_A<={1'b1,t_A[3:1]};
         else t_A<={1'b0,t_A[3:1]};
         
     always@(posedge con_clk,negedge control)
-        if(~control)t_B<=t_B;
+        if((~control)&(~reset))t_B<=4'b0000;//這行要放什麼是關鍵(是否需設定暫存器目前存取值 t_B<=B)
+        else if(~control)t_B<=t_B;
         else t_B<={in_B,t_B[3:1]};
 endmodule
--- HW4/HW4-1/tb_problem1.v
+++ HW4/HW4-1/tb_problem1.v
@@ -4,19 +4,22 @@ module tb_problem1;
     reg reset,clk,control;
     problem1 uut(A,B,in_B,control,reset,clk);
     initial #200 $finish;
-    always #10 clk=~clk;
+    always #5 clk=~clk;
     initial fork
         #0 reset=0;
         #0 control=0;
         #0 clk=0;
-        #0 in_B=0;
+        #0 in_B=1;
         #0 A=4'b0101;
         #0 B=4'b0111;
         #15 reset=1;
-        #25 control=1;
+        #30 control=1;
+        #30 in_B=1;
+        #40 in_B=0;
         #50 in_B=1;
-        #100 in_B=0;
-        #150 reset=0;
+        #60 in_B=0;
+        #100 in_B=1;
+        #130 in_B=0;
     join
     
 endmodule
